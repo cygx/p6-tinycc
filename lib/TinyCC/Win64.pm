9873793c472499576a3db22d558ac525603a3bac
cygx <cygx@cpan.org>
2017-05-12 00:00:29 +0200

    add README and copyright notices

--- lib/TinyCC/Win64.pm
+++ lib/TinyCC/Win64.pm
@@ -1,3 +1,6 @@
+# Copyright 2017 cygx <cygx@cpan.org>
+# Distributed under the Boost Software License, Version 1.0
+
 use TinyCC::Resources::Win64::DLL;
 sub EXPORT {
     TinyCC::Resources::Win64::DLL.setenv;
